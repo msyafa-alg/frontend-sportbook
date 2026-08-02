0f58344fb849583bd0e033b2423c0a0e06f1a0ed
msyafa-alg <m.syafaalgiffarifirdaus@smkwikrama.sch.id>
2026-08-03 01:57:57 +0700

    fix: remove stray closing JSX in navbar (profile dropdown)

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -109,7 +109,6 @@ export default function NavbarComponent() {
                             </Link>
                         </div>
                     )}
-                )}
                 </div>
             </div>
 
